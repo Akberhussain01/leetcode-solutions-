389349fbd9e0c03be78551c18548a9f0d3924d90
Mohammed akber hussain <274888306+Akberhussain01@users.noreply.github.com>
2026-04-24 00:20:58 +0530

    Create 25-combination-sum-ii.py

---
+++ medium/25-combination-sum-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def combinationSum2(self, candidates, target):
+        candidates.sort()
+        result = []
+        
+        def backtrack(start, path, remaining):
+            if remaining == 0:
+                result.append(path[:])
+                return
+            
+            if remaining < 0:
+                return
+            
+            for i in range(start, len(candidates)):
+                
+                # skip duplicates
+                if i > start and candidates[i] == candidates[i - 1]:
+                    continue
+                
+                path.append(candidates[i])
+                
+                # i + 1 क्योंकि reuse allowed नहीं
+                backtrack(i + 1, path, remaining - candidates[i])
+                
+                path.pop()
+        
+        backtrack(0, [], target)
+        return result
