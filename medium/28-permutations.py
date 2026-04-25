70720a144417736f49256fdb7cd9eef75b03cd2c
Mohammed akber hussain <274888306+Akberhussain01@users.noreply.github.com>
2026-04-26 04:09:00 +0530

    Create 28-permutations.py

---
+++ medium/28-permutations.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permute(self, nums):
+        result = []
+        
+        def backtrack(path, used):
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
+            
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+                
+                path.append(nums[i])
+                used[i] = True
+                
+                backtrack(path, used)
+                
+                path.pop()
+                used[i] = False
+        
+        used = [False] * len(nums)
+        backtrack([], used)
+        
+        return result
