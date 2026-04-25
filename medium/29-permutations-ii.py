625778d720ae9a566fa44c1a567e8feab32bc0b1
Mohammed akber hussain <274888306+Akberhussain01@users.noreply.github.com>
2026-04-26 04:10:38 +0530

    Create 29-permutations-ii.py

---
+++ medium/29-permutations-ii.py
@@ -0,0 +1,30 @@
+class Solution:
+    def permuteUnique(self, nums):
+        nums.sort()
+        result = []
+        used = [False] * len(nums)
+        
+        def backtrack(path):
+            if len(path) == len(nums):
+                result.append(path[:])
+                return
+            
+            for i in range(len(nums)):
+                
+                if used[i]:
+                    continue
+                
+                # skip duplicates
+                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                    continue
+                
+                path.append(nums[i])
+                used[i] = True
+                
+                backtrack(path)
+                
+                path.pop()
+                used[i] = False
+        
+        backtrack([])
+        return result
