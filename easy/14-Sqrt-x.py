18400f40813c1bc832055bbdff2718d35074d898
Mohammed akber hussain <274888306+Akberhussain01@users.noreply.github.com>
2026-04-30 00:30:30 +0530

    Create 14-Sqrt-x.py

---
+++ easy/14-Sqrt-x.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def mySqrt(self, x):
+        if x < 2:
+            return x
+
+        left, right = 1, x
+        answer = 0
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if mid * mid == x:
+                return mid
+            elif mid * mid < x:
+                answer = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return answer
